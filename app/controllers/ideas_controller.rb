497e93f4d6e4ff6471136b6a33ce6c787ca39c6e
RobinYanNaing <yannaingkyawtint@gmail.com>
2018-10-16 13:30:18 +0630

    first commit

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -10,6 +10,8 @@ class IdeasController < ApplicationController
   # GET /ideas/1
   # GET /ideas/1.json
   def show
+  @comments = @idea.comments.all
+  @comment = @idea.comments.build
   end
 
   # GET /ideas/new
@@ -28,7 +30,7 @@ class IdeasController < ApplicationController
 
     respond_to do |format|
       if @idea.save
-        format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
+        format.html { redirect_to @idea, notice: 'Your Recipe was successfully created.' }
         format.json { render :show, status: :created, location: @idea }
       else
         format.html { render :new }
@@ -42,7 +44,7 @@ class IdeasController < ApplicationController
   def update
     respond_to do |format|
       if @idea.update(idea_params)
-        format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
+        format.html { redirect_to @idea, notice: 'Recipe was successfully updated.' }
         format.json { render :show, status: :ok, location: @idea }
       else
         format.html { render :edit }
